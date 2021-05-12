afec7ba8246e62f934ebcfb05d48c8d6d3b6f37c
deepshah1309 <deepshah1309@gmail.com>
2021-05-13 00:15:33 +0530

    deploy2

--- client/src/Components/Main.js
+++ client/src/Components/Main.js
@@ -4,11 +4,11 @@ import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
+
 import DialogTitle from '@material-ui/core/DialogTitle';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { useTheme } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
+
 
 
 import { MDBDataTableV5 } from 'mdbreact';
